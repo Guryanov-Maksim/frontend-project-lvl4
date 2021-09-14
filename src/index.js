550223b93c3cd7861b3d9c73b316eb58990864e5
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-09-14 16:19:49 +0300

    feat: add rendering of the main logic
    rus: добавляет рендер основной логики

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 // @ts-check
 
-import 'core-js/stable/index.js';
-import 'regenerator-runtime/runtime.js';
-
+// import 'core-js/stable/index.js';
+// import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const p = document.createElement('p');
-p.classList.add('card-text');
-p.textContent = 'It works!';
+import Component from './components/App.jsx';
 
-const h5 = document.createElement('h5');
-h5.classList.add('card-title');
-h5.textContent = 'Project frontend l4 boilerplate';
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-cardBody.append(h5, p);
-
-const card = document.createElement('div');
-card.classList.add('card', 'text-center');
-card.append(cardBody);
-
-const container = document.querySelector('#chat');
-container.append(card);
-
-console.log('it works!');
+ReactDOM.render(
+  <Component />,
+  document.getElementById('chat'),
+);

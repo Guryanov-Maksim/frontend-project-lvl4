a6552834550619b1dec2d29658126df2a5a88ac9
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-09-19 17:05:05 +0300

    refactor: comment code that I don't know for what
    rus: комментирует код который я не знаю для чего

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 // @ts-check
 
-import 'core-js/stable/index.js'; // зачем эта зависимость?
+// import 'core-js/stable/index.js'; // зачем эта зависимость?
 import 'regenerator-runtime/runtime.js'; // разобраться почему когда коментишь регенератор, но появляется CORS проблемы
 import '../assets/application.scss';
 import ReactDOM from 'react-dom';

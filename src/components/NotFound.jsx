3013d50bde785ea954252b520775f9b1d16247d9
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-09-19 16:47:41 +0300

    feat: add data fetching from server
    rus: добавяет получение данных с сервера

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const NotFound = () => <p>Welcome to The Not Found page</p>;
+import routes from '../routes.js';
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
+
+const NotFound = () => {
+  const [content, setContent] = useState('');
+  useEffect(() => {
+    const fetchContent = async () => {
+      const { data } = await axios.get(routes.contentPath(), { headers: getAuthHeader() });
+      setContent(data);
+    };
+
+    fetchContent();
+  }, []);
+  console.log(content);
+  return <p>Welcome to The Not Found page</p>;
+};
 
 export default NotFound;

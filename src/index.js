dc4e113c02be21205894cd2115fa5ffb7f9d28bc
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-10-07 18:46:32 +0300

    fix: render

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const rollbarConfig = {
 };
 
 // сюда нужно вынести вебсокеты
-const chat = document.getElementById('chat')
+const chat = document.querySelector('#chat');
 console.log(chat);
 
 ReactDOM.render(

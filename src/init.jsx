5fb66a4bacc39e4c2a60a42ad57dcb2464b73b7f
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-10-11 12:32:10 +0300

    feat: enable roolbar only in prodaction
    rus: активирует rollbar только в продакшене

--- src/init.jsx
+++ src/init.jsx
@@ -25,6 +25,7 @@ if (process.env.NODE_ENV !== 'production') {
 const rollbarConfig = {
   accessToken: 'bc37e9074f544386aa0bf55a590eda16',
   environment: 'production',
+  enabled: process.env.NODE_ENV === 'production',
 };
 
 // сюда нужно вынести вебсокеты

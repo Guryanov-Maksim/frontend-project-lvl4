0426ea32e09bf61aa24fff8aa35e10284f607ec4
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-10-12 19:26:57 +0300

    refactor: replace Formik with useFormik()

--- src/features/messages/MessagesList.jsx
+++ src/features/messages/MessagesList.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { Formik, Field, Form } from 'formik';
+import React, { useContext, useEffect, useRef } from 'react';
+import { useFormik } from 'formik';
+import { Form, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
@@ -15,35 +16,58 @@ const MessageExcerpt = ({ message }) => (
 
 const MessageForm = () => {
   const ws = useContext(wsContext);
-  const [messageStatus, setMessageStatus] = useState('filling');
   const currentChannelId = useSelector(selectCurrentChannelId);
-  const [t] = useTranslation();
+  const { t } = useTranslation();
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      text: '',
+    },
+    onSubmit: (values, actions) => {
+      console.log(actions);
+      const { username } = JSON.parse(localStorage.getItem('userId'));
+      const message = {
+        text: values.text,
+        channelId: currentChannelId,
+        username,
+      };
+      const callbacks = {
+        onSuccess: [
+          () => actions.resetForm(),
+          () => inputRef.current.focus(),
+        ],
+        onFail: [
+          () => actions.setSubmitting(false),
+          () => inputRef.current.focus(),
+        ],
+      };
+      ws.sendMessage(message, callbacks);
+    },
+  });
+
+  console.log(formik);
 
   return (
     <div className="mt-auto px-5 py-3">
-      <Formik
-        initialValues={{
-          text: '',
-        }}
-        onSubmit={(values, actions) => {
-          const { username } = JSON.parse(localStorage.getItem('userId'));
-          const message = {
-            text: values.text,
-            channelId: currentChannelId,
-            username,
-          };
-          ws.sendMessage(message, { setMessageStatus, actions });
-        }}
-      >
-        {({ values }) => (
-          <Form>
-            <Field name="text" data-testid="new-message" placeholder={t('messages.placeholder')} />
-            <button className="btn btn-group-vertical" disabled={values.text === '' || messageStatus === 'sending'} type="submit">
-              <span className="visually-hidden">{t('messages.submitButton')}</span>
-            </button>
-          </Form>
-        )}
-      </Formik>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Control
+          type="text"
+          name="text"
+          value={formik.values.text}
+          onChange={formik.handleChange}
+          data-testid="new-message"
+          placeholder={t('messages.placeholder')}
+          ref={inputRef}
+        />
+        <Button className="btn btn-group-vertical" disabled={formik.values.text === '' || formik.isSubmitting} type="submit">
+          {t('messages.submitButton')}
+        </Button>
+      </Form>
     </div>
   );
 };

849bfa08affdfccd98acd02f6249371e70a3ca53
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-10-19 16:27:24 +0300

    refactor: remove unused code

--- src/init.jsx
+++ src/init.jsx
@@ -16,7 +16,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const rollbarConfig = {
-  // accessToken: 'bc37e9074f544386aa0bf55a590eda16',
   accessToken: process.env.ROLLBAR_TOKEN,
   environment: 'production',
   enabled: process.env.NODE_ENV === 'production',

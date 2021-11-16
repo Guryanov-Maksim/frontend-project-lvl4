cd0ceb67c77f0f4493cfa19206caf624583de71d
Guryanov Maksim <guryanov-mv@yandex.ru>
2021-11-16 08:51:16 +0300

    fix: remove doubling in HTML layout

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -5,27 +5,21 @@ import {
   Row,
   Col,
 } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 
 import SignupForm from '../components/SignupForm.jsx';
 
-const SignUpPage = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Container className="h-100">
-      <Row className="justify-content-md-center align-content-center h-100">
-        <Col md={8} xl={6}>
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Title>{t('signupPage.title')}</Card.Title>
-              <SignupForm />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+const SignUpPage = () => (
+  <Container className="h-100">
+    <Row className="justify-content-md-center align-content-center h-100">
+      <Col md={8} xl={6}>
+        <Card className="text-center">
+          <Card.Body>
+            <SignupForm />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default SignUpPage;
